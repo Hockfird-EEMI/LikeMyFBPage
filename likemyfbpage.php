58aa6ec6e79d7e60b748a49b73c5b2ec94c1d55e
Grégoire Sayer <gregasayer@gmail.com>
2016-10-17 16:46:28 +0200

    Page de début

---
+++ likemyfbpage.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name : LikeMyPage
+Plugin URI : http://localhost:8888/2A_DEV/EEMI/wordpress/wp-content/plugins/My-plugin/myplugin.php
+Description: Plugin encore en recherche de son utilité
+Version: Beta
+Author: Grégoire Sayer
+Author URI : https://github.com/Hockfird-EEMI
+License : GPL3
+License URI : https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+?>
